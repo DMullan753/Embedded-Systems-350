ac097535d0da4a28e7cd6f187b0d73ac8d591925
DMullan753 <dillon.mullan@students.plymouth.ac.uk>
2022-10-14 12:22:58 +0100

    Added stuff

--- Tasks/Task-212-PassingParams/main.cpp
+++ Tasks/Task-212-PassingParams/main.cpp
@@ -12,6 +12,42 @@ ComplexNumber_C complexAdd(const ComplexNumber_C a, const ComplexNumber_C b) {
     return y;
 }
 
+ComplexNumber_C complexConjugate(const ComplexNumber_C a, const ComplexNumber_C b) {
+    ComplexNumber_C y = a;
+
+    return y;
+}
+
+ComplexNumber_C complexNegate(const ComplexNumber_C a, const ComplexNumber_C b) {
+    ComplexNumber_C y = a;
+
+    return y;
+}
+
+ComplexNumber_C complexSubtract(const ComplexNumber_C a, const ComplexNumber_C b) {
+    ComplexNumber_C y = a;
+
+    return y;
+}
+
+ComplexNumber_C complexMagnitude(const ComplexNumber_C a, const ComplexNumber_C b) {
+    ComplexNumber_C y = a;
+
+    return y;
+}
+
+ComplexNumber_C complexMultiply(const ComplexNumber_C a, const ComplexNumber_C b) {
+    ComplexNumber_C y = a;
+
+    return y;
+}
+
+ComplexNumber_C complexDivide(const ComplexNumber_C a, const ComplexNumber_C b) {
+    ComplexNumber_C y = a;
+
+    return y;
+}
+
 void complexDisplay(const char *strName, const ComplexNumber_C u) {
     printf("%s = %f + j%f\n", strName, u.real, u.imag);
 }
